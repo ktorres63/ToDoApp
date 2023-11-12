49f9052d92e163a7c9f8f59ce68567013b2a9543
ktorres63 <ktorresaro@unsa.edu.pe>
2023-11-11 20:45:41 -0500

    add checkboxes and functionality

--- app/src/main/java/com/tutorial/todoapp/CategoriesAdapter.kt
+++ app/src/main/java/com/tutorial/todoapp/CategoriesAdapter.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class CategoriesAdapter(private val categories: List<TaskCategory>): RecyclerView.Adapter<CategoriesViewHolder>() {
+class CategoriesAdapter(private val categories: List<TaskCategory>, private val onItemSelected:(Int)-> Unit): RecyclerView.Adapter<CategoriesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_task_category, parent, false)
@@ -14,7 +14,7 @@ class CategoriesAdapter(private val categories: List<TaskCategory>): RecyclerVie
     override fun getItemCount() = categories.size
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
-        holder.render(categories[position])
+        holder.render(categories[position], onItemSelected)
     }
 
 
